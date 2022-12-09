82827a393694dfd3655b6cdfea34362edbf41f64
Mahabubur Rahman <mrmafi32@gmail.com>
2022-12-10 01:18:44 +0600

    banner-section

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Image } from "react-bootstrap";
+import banner from "../assests/slider.png";
+import './Banner.css'
 
 const Banner = () => {
-    return (
-        <div>
-            <h1>this is banner</h1>
-        </div>
-    );
+  return (
+    <div className="banner-section">
+      <div className="container text-white text-section">
+        <h1 className="fs-lg-1 fs-md-2 fs-sm-4 mb-0">Computer Engineering</h1>
+        <p>142,765 Computer Engineers Follow this</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+      <Container className='text-center py-3'>
+        <p>&copy; All services by ATG</p>
+      </Container>
     );
 };
 
