fc64c9c7db6192b6656ac85cd43a14356267f15c
Mahabubur Rahman <mrmafi32@gmail.com>
2022-12-10 01:31:49 +0600

    slider update

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Image } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 import banner from "../assests/slider.png";
-import './Banner.css'
+import "./Banner.css";
 
 const Banner = () => {
   return (
     <div className="banner-section">
+      <div className="text-end d-md-none pt-3 pe-4">
+        <Button variant="outline-light" >Join Group</Button>
+      </div>
       <div className="container text-white text-section">
         <h1 className="fs-lg-1 fs-md-2 fs-sm-4 mb-0">Computer Engineering</h1>
         <p>142,765 Computer Engineers Follow this</p>
